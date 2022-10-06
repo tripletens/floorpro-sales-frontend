33c8b1326ef70ee82c023545313b26878dde14d5
Kalu Chinonso <tripletens.kc@gmail.com>
2022-10-06 19:14:32 +0100

    added the dashboard table

---
+++ floorproadmin/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
+import { Login } from "./pages/login/login";
+import { AdminDashboard } from "./pages/dashboard/admin/pages/dashboard/index";
+import { ForgotPassword } from "./pages/ForgotPassword/index";
+import { Salesorder } from "./pages/dashboard/admin/pages/SalesOrder";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/forgotPassword" element={<ForgotPassword/>} />
+        <Route path="/admindashboard" element={<AdminDashboard/>} />
+        <Route path="/salesorder" element={<Salesorder/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
